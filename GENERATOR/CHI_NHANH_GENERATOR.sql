be9b5d7632c277eb9353c21e55827dd3614c78bb
DAT_EMPEROR <buidat161025a@gmail.com>
2025-12-26 22:33:54 +0700

    database and data generation

---
+++ GENERATOR/CHI_NHANH_GENERATOR.sql
@@ -0,0 +1,13 @@
+USE PETCAREX
+GO
+ 
+insert into CHI_NHANH (MA_CHI_NHANH, TEN, SO_NHA, QUAN, HUYEN, THOI_GIAN_MO_CUA, THOI_GIAN_DONG_CUA, DIEM_DANH_GIA) values ('02', 'petcare 2', 789, 'quận 9', 'Nhà Bè', '9:00', '19:00', 10);
+insert into CHI_NHANH (MA_CHI_NHANH, TEN, SO_NHA, QUAN, HUYEN, THOI_GIAN_MO_CUA, THOI_GIAN_DONG_CUA, DIEM_DANH_GIA) values ('07', 'petcare 3', 246, 'quận 4', 'Củ Chi', '10:00', '18:00', 8);
+insert into CHI_NHANH (MA_CHI_NHANH, TEN, SO_NHA, QUAN, HUYEN, THOI_GIAN_MO_CUA, THOI_GIAN_DONG_CUA, DIEM_DANH_GIA) values ('06', 'petcare 1', 135, 'quận 10', 'Hóc Môn', '10:00', '19:00', 4);
+insert into CHI_NHANH (MA_CHI_NHANH, TEN, SO_NHA, QUAN, HUYEN, THOI_GIAN_MO_CUA, THOI_GIAN_DONG_CUA, DIEM_DANH_GIA) values ('03', 'petcare 3', 579, 'quận 6', 'Cần Giờ', '9:00', '19:00', 5);
+insert into CHI_NHANH (MA_CHI_NHANH, TEN, SO_NHA, QUAN, HUYEN, THOI_GIAN_MO_CUA, THOI_GIAN_DONG_CUA, DIEM_DANH_GIA) values ('09', 'petcare 8', 456, 'quận 8', 'Hóc Môn', '8:00', '20:00', 3);
+insert into CHI_NHANH (MA_CHI_NHANH, TEN, SO_NHA, QUAN, HUYEN, THOI_GIAN_MO_CUA, THOI_GIAN_DONG_CUA, DIEM_DANH_GIA) values ('08', 'petcare 8', 135, 'quận 2', 'Bình Chánh', '10:00', '20:00', 4);
+insert into CHI_NHANH (MA_CHI_NHANH, TEN, SO_NHA, QUAN, HUYEN, THOI_GIAN_MO_CUA, THOI_GIAN_DONG_CUA, DIEM_DANH_GIA) values ('10', 'petcare 6', 321, 'quận 2', 'Củ Chi', '10:00', '20:00', 6);
+insert into CHI_NHANH (MA_CHI_NHANH, TEN, SO_NHA, QUAN, HUYEN, THOI_GIAN_MO_CUA, THOI_GIAN_DONG_CUA, DIEM_DANH_GIA) values ('04', 'petcare 4', 246, 'quận 5', 'Bình Chánh', '8:00', '19:00', 10);
+insert into CHI_NHANH (MA_CHI_NHANH, TEN, SO_NHA, QUAN, HUYEN, THOI_GIAN_MO_CUA, THOI_GIAN_DONG_CUA, DIEM_DANH_GIA) values ('05', 'petcare 1', 987, 'quận 9', 'Bình Chánh', '9:00', '19:00', 2);
+insert into CHI_NHANH (MA_CHI_NHANH, TEN, SO_NHA, QUAN, HUYEN, THOI_GIAN_MO_CUA, THOI_GIAN_DONG_CUA, DIEM_DANH_GIA) values ('01', 'petcare 6', 987, 'quận 2', 'Hóc Môn', '10:00', '18:00', 2);
---
+++ GENERATOR/DANH_MUC_DICH_VU_GENERATOR.sql
@@ -0,0 +1,4 @@
+insert into DANH_MUC_DICH_VU (LOAI_DICH_VU, TEN_DICH_VU) values ('01', 'khám bệnh');
+insert into DANH_MUC_DICH_VU (LOAI_DICH_VU, TEN_DICH_VU) values ('02', 'mua hàng');
+insert into DANH_MUC_DICH_VU (LOAI_DICH_VU, TEN_DICH_VU) values ('03', 'tiêm phòng');
+insert into DANH_MUC_DICH_VU (LOAI_DICH_VU, TEN_DICH_VU) values ('04', 'chăm sóc thú cưng');
